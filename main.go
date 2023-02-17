3858b10d5e867c03ebd163b1e2640d9ee38e50a7
sratna <adhikariratna52@gmail.com>
2023-02-17 12:54:15 +0100

    add get bikes endpoint

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/bike-sharing-app/handlers"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -12,6 +13,8 @@ func main() {
 	app.Get("/api/hello", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
 	})
+
+	app.Get("api/bikes", handlers.GetAllBikes)
 
 	log.Fatal(app.Listen(":3000"))
 }
