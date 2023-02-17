4dc17201ad3679068acccef1a90592927f995c5b
sratna <adhikariratna52@gmail.com>
2023-02-17 14:23:21 +0100

    add basic layout to get started

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,48 @@
-function App() {
-  return (
-    <div>
-      Simple Bike Sharing App
-    </div>
-  )
-}
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
+const { Header, Content, Footer } = Layout;
+import './App.css';
 
-export default App
+const App = () => {
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+  return (
+    <Layout className="layout">
+      <Header>
+        <div className="logo">
+          Bike Sharing App
+        </div>
+      </Header>
+      <Content
+        style={{
+          padding: '0 50px',
+        }}
+      >
+        <Breadcrumb
+          style={{
+            margin: '16px 0',
+          }}
+        >
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>Bikes</Breadcrumb.Item>
+        </Breadcrumb>
+        <div
+          className="site-layout-content"
+          style={{
+            background: colorBgContainer,
+          }}
+        >
+          Content
+        </div>
+      </Content>
+      <Footer
+        style={{
+          textAlign: 'center',
+        }}
+      >
+        Bike Sharing App ©2023 Created by SRatna
+      </Footer>
+    </Layout>
+  );
+};
+export default App;
